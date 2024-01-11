dacf37f0f9a3b77bbd277eb77bb1d7e879ed1e82
Kiet Le <letankietpdcd@gmail.com>
2024-01-11 21:37:04 +0700

    Add grading management

---
+++ frontend/src/pages/user/ClassDetail/Stream.tsx
@@ -0,0 +1,36 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+import React from "react";
+
+interface StreamProps {
+  currentClass: any;
+}
+
+const Stream = ({ currentClass }: StreamProps) => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage:
+            "url(https://www.gstatic.com/classroom/themes/img_read.jpg)",
+          backgroundSize: "cover",
+        }}
+        className="h-[240px] relative mb-6 rounded-lg"
+      >
+        <div className="absolute bottom-6 left-6 text-white">
+          <h2 className="text-4xl font-semibold uppercase">
+            {currentClass?.title}
+          </h2>
+          <p className="font-normal text-2xl">{currentClass?.description}</p>
+        </div>
+      </div>
+
+      <div className="flex gap-4 items-center px-5 py-3 shadow-main rounded-lg">
+        <Avatar size="large" icon={<UserOutlined />} />
+        <span>Announce something to your class</span>
+      </div>
+    </div>
+  );
+};
+
+export default Stream;
--- frontend/src/pages/user/ClassDetail/index.tsx
+++ frontend/src/pages/user/ClassDetail/index.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import userApi from "../../../api/userApi";
 import { UserOutlined } from "@ant-design/icons";
 import { Role } from "../../../utils/enum";
+import Stream from "./Stream";
+import People from "./People";
 
 const ClassDetail = () => {
   const { id } = useParams();
@@ -27,32 +29,7 @@ const ClassDetail = () => {
     {
       key: "1",
       label: "Stream",
-      children: (
-        <div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://www.gstatic.com/classroom/themes/img_read.jpg)",
-              backgroundSize: "cover",
-            }}
-            className="h-[240px] relative mb-6 rounded-lg"
-          >
-            <div className="absolute bottom-6 left-6 text-white">
-              <h2 className="text-4xl font-semibold uppercase">
-                {currentClass?.title}
-              </h2>
-              <p className="font-normal text-2xl">
-                {currentClass?.description}
-              </p>
-            </div>
-          </div>
-
-          <div className="flex gap-4 items-center px-5 py-3 shadow-main rounded-lg">
-            <Avatar size="large" icon={<UserOutlined />} />
-            <span>Announce something to your class</span>
-          </div>
-        </div>
-      ),
+      children: <Stream currentClass={currentClass} />,
     },
     {
       key: "2",
